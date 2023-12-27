896e4b31f8b302efa8c2f607d70da8219de222f7
mingfei <mingfei@tsinghua.edu.cn>
2023-12-27 12:30:07 +0800

    Initial commit

---
+++ routes/image.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+const {OpenAI} = require('openai')
+
+const openai = new OpenAI({
+    apiKey: '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'
+})
+
+router.post('/', (req, res) => {
+    gen(req, res);
+});
+
+
+async function gen(req, res) {
+    const {prompt, size} = req.body;
+    console.log(prompt, size);
+
+    const response = await openai.images.generate({
+        prompt: prompt,
+        size: size,
+        model: 'dall-e-3',
+        quality: 'hd',
+        style: 'natural'
+    });
+    let url = response.data[0].url;
+    console.log(response.data);
+    res.status(200).json({
+        success: true,
+        data: url
+    })
+}
+
+
+module.exports = router;
